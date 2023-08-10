24f60c07134d0e1b4a1065e1b5a7beb9c7c922dd
jeecode14 <jaysoncanete97@gmail.com>
2023-08-10 17:08:46 +0800

    Remove all tracked files

--- recover.js
+++
@@ -1,8 +0,0 @@
-// recover.js
-//import 'dotenv/config'
-
-const bs58 = require('bs58');
-const fs = require('fs');
-b = bs58.decode('5DgN3M9xEMw4B4Lj6ZoERcudodtcWSH3sJcspuQYBZi2nD13ufWqfbL6hyBgp4XAA6zGp3t3EUgr2URcfp2u8FLt');
-j = new Uint8Array(b.buffer, b.byteOffset, b.byteLength / Uint8Array.BYTES_PER_ELEMENT);
-fs.writeFileSync('key.json', `[${j}]`);
